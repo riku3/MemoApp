11c8b32b6ece1a8e3e2f973ccd0e41e624a45a99
riku <riku@rikunoMacBook-Pro.local>
2019-06-11 15:46:14 +0900

    Bug-fix keyboard

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 import firebase from 'firebase';
 
 import CircleButton from '../elements/CircleButton';
@@ -42,7 +42,7 @@ class MemoEditScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
         <TextInput
           style={styles.memoEditInput}
           multiline
@@ -54,7 +54,7 @@ class MemoEditScreen extends React.Component {
           name="check"
           onPress={this.handlePress.bind(this)}
         />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
